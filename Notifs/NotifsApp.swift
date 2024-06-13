4a3936108e2989e64de37da99e9c15877376638d
Ayush <ayushdesai1000@gmail.com>
2024-06-13 18:36:56 +0530

    Initial Commit

---
+++ Notifs/NotifsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifsApp.swift
+//  Notifs
+//
+//  Created by Ayush Desai on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NotifsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
